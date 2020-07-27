3631514f4ade0e8329879d7a9833e434e70297bd
Linh B Ngo <lngo@wcupa.edu>
2020-07-26 20:54:57 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -17,22 +17,11 @@ tour = IG.Tour()
 tour.Description(IG.Tour.TEXT,tourDescription)
 request.addTour(tour)
 
-prefixForIP = "192.168.1."
-link = request.LAN("lan")
-
-for i in range(3):
-  if i == 0:
-    node = request.XenVM("head")
-  else:
-    node = request.XenVM("worker-" + str(i))
-  node.cores = 4
-  node.ram = 8192
-  node.routable_control_ip = "true" 
-  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
-  iface = node.addInterface("if" + str(i))
-  iface.component_id = "eth1"
-  iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
-  link.addInterface(iface)
-  node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_docker.sh"))
+node = request.XenVM("head")
+node.cores = 4
+node.ram = 8192
+node.routable_control_ip = "true" 
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
+node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_docker.sh"))
   
 pc.printRequestRSpec(request)
